e7c518473a4ee15371be61db148c7dd96558fea3
Adithya Narasinghe <adithyasnarasinghe@gmail.com>
2021-02-04 06:40:31 +0530

    WIP: Adding cover image & code samples

--- js/site.js
+++ js/site.js
@@ -7,3 +7,17 @@ function triggerNavBar() {
         x.className = "navbar";
     }
 }
+
+function openSample(target, framework) {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(framework).style.display = "block";
+    target.className += " active";
+}
